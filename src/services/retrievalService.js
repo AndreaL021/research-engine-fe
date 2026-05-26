6e3b04604ef10859e11b6fb98e7979ba61537df0
AndreaL021 <lopassoandrea1@gmail.com>
2026-05-26 18:37:36 +0200

    added .env files and api url

--- src/services/retrievalService.js
+++ src/services/retrievalService.js
@@ -1,5 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL
+
 export async function retrieveDocuments (query) {
-    const response = await fetch("http://127.0.0.1:8000/research", {
+    const response = await fetch(API_URL+"/research", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
